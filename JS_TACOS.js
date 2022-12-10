57e3ab4f5adddd963776506de22e8f14334747b5
raya <96365644+rayanawas@users.noreply.github.com>
2022-12-10 14:47:11 +0100

    Add files via upload

--- JS_TACOS.js
+++ JS_TACOS.js
@@ -1,4 +1,4 @@
-function myFunction(){
+/*function myFunction(){
     var editor = document.getElementById ('textarea');
     var button = document.getElementById('share');
     
@@ -6,5 +6,21 @@ function myFunction(){
     editor.setSelectionRange(0,99999);
     navigator.clipboard.writeText(textarea.value);
     alert('copié!');
+}*/
+
+
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copié " + copyText.value);
 }
 
